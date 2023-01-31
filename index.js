659a6aea01a70cf42eea49c0a31bb1e906816227
Mai Nhật Hoàng <62861666+MaiNhatHoangY2001@users.noreply.github.com>
2023-01-31 17:34:19 +0700

    Add test SSO in index.js

--- index.js
+++ index.js
@@ -5,11 +5,13 @@ const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const dotenv = require("dotenv");
 const cookieParser = require("cookie-parser");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20");
 
 const app = express();
 
 //ROUTES
-const testRoute = require("./routes/test");
+const authRoute = require('./routes/authEmp');
 
 const port = process.env.PORT || 8000;
 const origin =
@@ -23,6 +25,19 @@ mongoose.connect(process.env.MONGODB_URL, () => {
 	console.log('Connect to MongoDB');
 });
 
+//SSO
+passport.use(new GoogleStrategy(
+  {
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: '/auth/google/callback'
+  },
+  accessToken => {
+    console.log(accessToken);
+  }
+));
+
+
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(
@@ -34,8 +49,17 @@ app.use(
 app.use(morgan("common"));
 
 //ROUTERS
-app.use("/api", testRoute);
+app.use("/api", authRoute);
 
 const server = app.listen(port, () => {
   console.log(`server is running... at ${port}`);
 });
+
+//TESTING
+app.get(
+  '/auth/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email']
+  })
+);
+app.get('/auth/google/callback', passport.authenticate('google'));
