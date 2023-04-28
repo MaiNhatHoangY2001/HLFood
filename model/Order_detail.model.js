4203d557791ede03e65dcbb7690e3461d127963d
MaiNhatHoangY2001 <hoan44pro@gmail.com>
2023-04-28 21:47:44 +0700

    FA-112-Need-API-finish-food-from-cooker

--- model/Order_detail.model.js
+++ model/Order_detail.model.js
@@ -1,52 +1,54 @@
-const mongoose = require("mongoose");
-const FoodModel = require("./Food.model");
-const OrderModel = require("./Order.model");
+const mongoose = require('mongoose');
+const FoodModel = require('./Food.model');
+const OrderModel = require('./Order.model');
 
 const order_detail = new mongoose.Schema({
-    time_created: {
-        type: Date,
-        default: Date.now,
-    },
-    quantity: {
-        type: Number,
-        default: 1,
-    },
-    total_detail_price: {
-        type: Number,
-        default: 0
-    },
-    description: {
-        type: String,
-    },
-    status: {
-        type: Number,
-        default: 0
-    },
-    food:
-    {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Food",
-    },
-    order: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Order",
-    }
-})
-
-order_detail.pre('save', async function (next) {
-    try {
-        //Save total_detail_price
-        const food = await FoodModel.findById(this.food);
-        this.total_detail_price = food.price * this.quantity;
-        //Save total_order_price in order
-        const order = await OrderModel.findById(this.order);
-        order.total_order_price += this.total_detail_price + this.total_detail_price * order.vat;
-        await order.save();
-        next();
-    } catch (err) {
-        next(err);
-    }
+	time_created: {
+		type: Date,
+		default: Date.now,
+	},
+	quantity: {
+		type: Number,
+		default: 1,
+	},
+	quantity_finished: {
+		type: Number,
+		default: 0,
+	},
+	total_detail_price: {
+		type: Number,
+		default: 0,
+	},
+	description: {
+		type: String,
+	},
+	status: {
+		type: Number,
+		default: 0,
+	},
+	food: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'Food',
+	},
+	order: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'Order',
+	},
 });
 
+order_detail.pre('save', async function (next) {
+	try {
+		//Save total_detail_price
+		const food = await FoodModel.findById(this.food);
+		this.total_detail_price = food.price * this.quantity;
+		//Save total_order_price in order
+		const order = await OrderModel.findById(this.order);
+		order.total_order_price += this.total_detail_price + this.total_detail_price * order.vat;
+		await order.save();
+		next();
+	} catch (err) {
+		next(err);
+	}
+});
 
-module.exports = mongoose.model("OrderDetail", order_detail);
+module.exports = mongoose.model('OrderDetail', order_detail);
