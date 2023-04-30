1656ac4564f99ab68620519d83eaddb7d8010534
MaiNhatHoangY2001 <hoan44pro@gmail.com>
2023-04-30 10:35:26 +0700

    Populate Employee in table

--- controllers/table.controller.js
+++ controllers/table.controller.js
@@ -1,80 +1,74 @@
-const Table = require("../model/Table.model");
-const Employee = require("../model/Employee.model");
-const Order = require("../model/Order.model");
-
+const Table = require('../model/Table.model');
+const Employee = require('../model/Employee.model');
+const Order = require('../model/Order.model');
 
 const tableController = {
+	getAllTable: async (req, res) => {
+		try {
+			const listTable = await Table.find(req.query).populate('employee');
+			res.status(200).json(listTable);
+		} catch (error) {
+			res.status(500).json(error);
+		}
+	},
+	updateStatus: async (req, res) => {
+		try {
+			const table = Table.findById(req.query.id);
+			await table.updateOne({ $set: { status: req.body.status } });
+			res.status(200).json('Update successfully');
+		} catch (error) {
+			res.status(500).json(error);
+		}
+	},
+	setEmpTable: async (req, res) => {
+		try {
+			const empId = req.body.empId;
+			const tablesInput = req.body.tables.split(',').map(Number);
 
-    getAllTable: async (req, res) => {
-        try {
+			//Update employee in table
+			const listTableWithNum = Table.find({ table_num: { $in: tablesInput } });
+			await Table.updateMany({ table_num: { $in: tablesInput } }, { $set: { employee: empId } });
 
-            const listTable = await Table.find(req.query);
+			//Update tables in employee
+			const employee = Employee.findById(empId);
+			const arrayIds = await listTableWithNum.distinct('_id');
+			await employee.updateOne({ $set: { tables: arrayIds } });
 
-            res.status(200).json(listTable);
-        } catch (error) {
-            res.status(500).json(error);
-        }
-    },
-    updateStatus: async (req, res) => {
-        try {
-            const table = Table.findById(req.query.id);
-            await table.updateOne({ $set: { "status": req.body.status } });
-            res.status(200).json("Update successfully");
-        } catch (error) {
-            res.status(500).json(error);
-        }
-    },
-    setEmpTable: async (req, res) => {
-        try {
-            const empId = req.body.empId;
-            const tablesInput = req.body.tables.split(",").map(Number);
+			res.status(200).json('Update successfully');
+		} catch (error) {
+			res.status(500).json(error);
+		}
+	},
+	udpateTableOrder: async (req, res) => {
+		try {
+			const idOrder = req.body.order_id;
 
-            //Update employee in table
-            const listTableWithNum = Table.find({ "table_num": { "$in": tablesInput } });
-            await Table.updateMany({ "table_num": { "$in": tablesInput } }, { $set: { employee: empId } });
+			const orders = await Order.findById(idOrder);
+			const idTables = orders?.tables;
 
-            //Update tables in employee
-            const employee = Employee.findById(empId);
-            const arrayIds = await listTableWithNum.distinct("_id");
-            await employee.updateOne({ $set: { tables: arrayIds } })
+			await deleteTableOrder(idTables, idOrder);
 
-            res.status(200).json("Update successfully");
-        } catch (error) {
-            res.status(500).json(error);
-        }
-    },
-    udpateTableOrder: async (req, res) => {
-        try {
-            const idOrder = req.body.order_id;
+			await addTableOrder(req.body.table, idOrder, req.body.status);
 
-            const orders = await Order.findById(idOrder);
-            const idTables = orders?.tables;
-
-            await deleteTableOrder(idTables, idOrder)
-
-            await addTableOrder(req.body.table, idOrder, req.body.status)
-
-            res.status(200).json("Update successfully");
-        } catch (error) {
-            res.status(500).json(error);
-        }
-    }
+			res.status(200).json('Update successfully');
+		} catch (error) {
+			res.status(500).json(error);
+		}
+	},
 };
 
-
 async function deleteTableOrder(idTables, idOrder) {
-    for (const id of idTables) {
-        await Table.updateOne({ _id: id }, { $unset: { order: 1 }, $set: { status: 0 } })
-    }
-    await Order.updateOne({ _id: idOrder }, { $unset: { tables: 1 } })
+	for (const id of idTables) {
+		await Table.updateOne({ _id: id }, { $unset: { order: 1 }, $set: { status: 0 } });
+	}
+	await Order.updateOne({ _id: idOrder }, { $unset: { tables: 1 } });
 }
 
 async function addTableOrder(idTables, idOrder, status) {
-    for (const id of idTables) {
-        await Table.updateOne({ _id: id }, { $set: { order: idOrder, status: status } })
-    }
-    await Order.updateOne({ _id: idOrder }, { $set: { tables: [...idTables] } })
+	for (const id of idTables) {
+		await Table.updateOne({ _id: id }, { $set: { order: idOrder, status: status } });
+	}
+	await Order.updateOne({ _id: idOrder }, { $set: { tables: [...idTables] } });
 }
-
 
 module.exports = tableController;
