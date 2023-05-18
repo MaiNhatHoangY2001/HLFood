5467e0290ee54f8e2a511d0de1418688b9f0d0c1
MaiNhatHoangY2001 <hoan44pro@gmail.com>
2023-05-19 06:23:07 +0700

    FA-126 Finish create API management of tables

--- controllers/table.controller.js
+++ controllers/table.controller.js
@@ -3,9 +3,75 @@ const Employee = require('../model/Employee.model');
 const Order = require('../model/Order.model');
 
 const tableController = {
+	addTables: async (req, res) => {
+		try {
+			const numTables = parseInt(req.body.numTables);
+			const floor = parseInt(req.body.floor);
+			const chair = parseInt(req.body.chair);
+
+			const newTables = [];
+			for (let i = 0; i < numTables; i++) {
+				const tables = await Table.find();
+				const newTable = new Table(getTableMissing(tables, floor, chair));
+				const tableAdd = await newTable.save();
+				newTables.push(tableAdd);
+			}
+			res.status(200).json(newTables);
+		} catch (error) {
+			res.status(500).json(error);
+		}
+	},
+
+	updateNumTable: async (_req, res) => {
+		try {
+			const tables = await Table.find({ is_deleted: false }).sort('table_num is_deleted');
+
+			let currentNum = 1;
+			for (const table of tables) {
+				table.table_num = currentNum;
+				await Table.findByIdAndUpdate(table._id, table);
+				currentNum++;
+			}
+			const updateTables = await Table.find({ is_deleted: false }).sort('table_num is_deleted');
+
+			res.status(200).json(updateTables);
+		} catch (error) {
+			res.status(500).json(error);
+		}
+	},
+
+	updateTable: async (req, res) => {
+		try {
+			const table = Table.findById(req.query.id);
+			await table.updateOne({
+				$set: {
+					floor: req.body.floor,
+					chair: req.body.chair,
+				},
+			});
+			res.status(200).json('Update successfully');
+		} catch (error) {
+			res.status(500).json(error);
+		}
+	},
+
+	deleteTable: async (req, res) => {
+		try {
+			const table = Table.findById(req.query.id);
+			await table.updateOne({
+				$set: {
+					is_deleted: true,
+				},
+			});
+			res.status(200).json('Delete successfully');
+		} catch (error) {
+			res.status(500).json(error);
+		}
+	},
+
 	getAllTable: async (req, res) => {
 		try {
-			const listTable = await Table.find(req.query).populate('employee');
+			const listTable = await Table.find(req.query).populate('employee').sort('table_num is_deleted');
 			res.status(200).json(listTable);
 		} catch (error) {
 			res.status(500).json(error);
@@ -70,6 +136,23 @@ async function addTableOrder(idTables, idOrder, status) {
 		await Table.updateOne({ _id: id }, { $set: { order: idOrder, status: status } });
 	}
 	await Order.updateOne({ _id: idOrder }, { $set: { tables: [...idTables] } });
+}
+
+function getTableMissing(existingArray, floor, chair) {
+	const existingNums = existingArray.map((item) => item.table_num);
+	const maxNum = Math.max(...existingNums);
+	let newNum = maxNum + 1;
+
+	for (const item of existingArray) {
+		if (item.is_deleted) {
+			const isTableAdd = existingArray.filter((table) => table.table_num == item.table_num);
+			if (isTableAdd.length === 1) {
+				newNum = item.table_num;
+			}
+		}
+	}
+
+	return { table_num: newNum, status: 0, floor: floor, chair: chair };
 }
 
 module.exports = tableController;
