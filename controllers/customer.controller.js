96609fdf41d548dd86fa4a9b6a4aee70d0a1fee1
MaiNhatHoangY2001 <hoan44pro@gmail.com>
2023-05-22 02:04:21 +0700

    Hot Fix

--- controllers/customer.controller.js
+++ controllers/customer.controller.js
@@ -3,7 +3,12 @@ const CustomerModel = require('../model/Customer.model');
 const cusController = {
 	getAllCus: async (req, res) => {
 		try {
-			const cus = await CustomerModel.find(res.query).populate('order', '_id time_booking', null, { sort: { time_booking: -1 } });
+			const cus = await CustomerModel.find(res.query)
+				.populate('order', '_id tables time_booking', null, { sort: { time_booking: -1 } })
+				.populate({
+					path: 'order',
+					populate: { path: 'tables' },
+				});
 			res.status(200).json(cus);
 		} catch (error) {
 			res.status(500).json(error);
